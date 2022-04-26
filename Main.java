98f1697747541c810fa3636fff8415bf41c0d7cb
167727 <100698238+167727stud@users.noreply.github.com>
2022-04-26 13:34:45 +0000

    Commit 4_2

--- Main.java
+++ Main.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 
 class WrongStudentName extends Exception { }
 class WrongStudentAge extends Exception { }
+class WrongStudentDate extends Exception { }
+class WrongChoice extends Exception { }
+class StudentParseError extends Exception { }
 
 class Main {
+  
     public static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws WrongChoice {
+        
         while(true) {
             try {
                 int ex = menu();
@@ -16,7 +21,8 @@ class Main {
                     case 1: exercise1(); break;
                     case 2: exercise2(); break;
                     case 3: exercise3(); break;
-                    default: return;
+                    case 4: return;
+                    default: Choice();break;
                 }
             } catch(IOException e) {
 
@@ -26,6 +32,15 @@ class Main {
               catch(WrongStudentAge e) {
                 System.out.println("Błędny wiek studenta!");
             }
+              catch(WrongStudentDate e) {
+                System.out.println("Błędny data urodzenia!");
+            }
+              catch(WrongChoice e) {
+                System.out.println("zły wybór");
+            }
+              catch(StudentParseError e) {
+                System.out.println("StudentParseError");
+            }
         }
     }
 
@@ -34,7 +49,7 @@ class Main {
         System.out.println("1 - aby dodać studenta");
         System.out.println("2 - aby wypisać wszystkich studentów");
         System.out.println("3 - aby wyszukać studenta po imieniu");
-        System.out.println("0 - aby wyjść z programu");
+        System.out.println("4 - aby wyjść z programu");
         return scan.nextInt();
     }
 
@@ -55,14 +70,26 @@ class Main {
           
       return age;
     }
+      public static String ReadDate() throws WrongStudentDate {
+        scan.nextLine();
+        System.out.println("Podaj datę urodzenia DD-MM-YYY");
+        String Date = scan.nextLine();
+        if(Date.length() !=10 || !Date.contains("-"))
+            throw new WrongStudentDate();
+
+        return Date;
+        }
+        public static String Choice() throws WrongChoice {  
+            throw new WrongChoice();
+        }
 
   
 
-    public static void exercise1() throws IOException, WrongStudentName,WrongStudentAge {
+    public static void exercise1() throws IOException, WrongStudentName , WrongStudentAge, WrongStudentDate {
         var name = ReadName();
         var age = ReadAge();
-        System.out.println("Podaj datę urodzenia DD-MM-YYY");
-        var date = scan.nextLine();
+        
+        var date = ReadDate();
         (new Service1()).addStudent(new Student(name, age, date));
     }
 
